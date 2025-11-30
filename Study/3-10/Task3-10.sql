c13a5a26177c6b71c543da3dfe565a1022c98ba9
kobayashi723 <kobayashi7230601@gmail.com>
2025-11-30 12:24:45 +0900

    3-10

--- Study/3-10/Task3-10.sql
+++ Study/3-10/Task3-10.sql
@@ -1,5 +1,3 @@
-
-
 /* ■ [回答]と記載のある箇所へ、1〜8の各課題内容に沿ったSQL文を記述しなさい。 */
 
 -- 1. Staffテーブルから「経理部」に所属する社員の情報をすべて抽出してください。
@@ -8,17 +6,18 @@ SELECT * FROM staff WHERE section='経理部' ;
 
 -- 2. 在庫(Stocksテーブル)の在庫数(Quantity)が10以上25未満のものを抽出して下さい。
 SELECT * FROM Stocks
-WHERE Quantity >= 10 AND Quantity <= 25;
+WHERE Quantity >= 10 AND Quantity < 25;
 
 
 -- 3. INTERSECT演算子を使用して、Order_Headerテーブルで合計値(Total)が5000以上10000未満のものを抽出して下さい。
-SELECT * FROM Order_Header A WHERE A.Total > 5000
+SELECT * FROM Order_Header A WHERE A.Total >= 5000
 INTERSECT
 SELECT * FROM Order_Header B WHERE B.Total < 10000;
 
 
 -- 4. 「関東」エリアの全店舗情報を抽出して下さい。
-SELECT * FROM shop WHERE AreaCode='04';
+SELECT * FROM shop JOIN area ON shop.areacode = area.areacode
+where areaname='関東';
 
 
 -- 5. 在庫(Stocksテーブル)内の各商品の合計数量を抽出して下さい。
@@ -27,7 +26,7 @@ GROUP BY goodscode;
 
 
 -- 6. 商品（Gods）テーブルから単価（UnitPrice)が5000円より高い商品の情報を全て抽出して下さい。
-SELECT * from goods where 5000 <= UnitPrice ;
+SELECT * from goods where 5000 < UnitPrice ;
 
 
 -- 7. Shopテーブルの全ての店舗コード（Shopcode）、店舗名（Shopname）を、所在地（areaname）とあわせて抽出してください。
@@ -35,5 +34,6 @@ SELECT Shopcode,Shopname,areaname  FROM shop JOIN area ON shop.areacode = area.a
 
 
 -- 8. 「新宿」店の在庫数が10以上の商品の商品コード（GoodsCode）、商品名（GoodsName）、在庫数（quantity）を抽出して下さい。
-SELECT stocks.goodscode, Goodsname, quantity FROM stocks JOIN goods ON stocks.goodscode =goods.goodscode
-where shopcode = '001' and quantity >= 10;
+SELECT stocks.goodscode, goodsname, quantity FROM stocks JOIN shop ON stocks.shopcode =shop.shopcode
+join goods on stocks.GoodsCode = goods.GoodsCode
+where shopname = '新宿' and quantity >= 10;
