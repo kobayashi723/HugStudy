a5e4656f721ea58fde48eba77cfe1a083c20226e
kobayashi723 <kobayashi7230601@gmail.com>
2025-12-05 15:22:39 +0900

    Delete Study/11252022 directory

--- Study/11252022/src/Main.java
+++
@@ -1,9 +0,0 @@
-// calculatorパッケージ中のCalculatorクラスをimport
-import calculator.Calculator;
-
-public class Main {
-    public static void main(String[] args) {
-      // インポートしたCalculatorクラスのplusメソッドを使用して出力します。
-      System.out.println(Calculator.plus(10));
-    }
-}
--- Study/11252022/src/calculator/Calculator.java
+++
@@ -1,8 +0,0 @@
-// calculatorパッケージのCalculatorクラス
-package calculator;
-
-public class Calculator {
-    public static int plus(int a) {
-      return a + 1;
-    }
-}
